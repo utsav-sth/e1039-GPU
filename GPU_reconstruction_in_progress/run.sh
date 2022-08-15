fbe73dd3ad65c4173df203adf92cb6d7b572d91a
efuchey <efuchey@gmail.com>
2022-08-14 23:01:46 -0400

    Updated run.sh => I believe it should use online_reconstruction.cu

--- GPU_reconstruction_in_progress/run.sh
+++ GPU_reconstruction_in_progress/run.sh
@@ -1,1 +1,2 @@
-make && ./online_reconstruction_3d_linear_test/ "Name of file".root "Name of output file"
+#make && ./online_reconstruction_3d_linear_test/ "Name of file".root "Name of output file"
+make && ./online_reconstruction/ "Name of file".root "Name of output file"
