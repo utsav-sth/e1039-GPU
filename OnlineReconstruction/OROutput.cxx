2fda8e443e96c3e0294a8572e33e7f0d54f792d0
fermipdoc <efuchey@gmail.com>
2023-04-10 14:59:37 -0500

    Added all the necessary members for the root output class.

--- OnlineReconstruction/OROutput.cxx
+++ OnlineReconstruction/OROutput.cxx
@@ -22,22 +22,72 @@ void ORoutput_tree::Init()
   Clear();
   //fHitsReduced.clear();
   
-  fChain->SetBranchAddress("Nreducedhits[55]", fNhitsReduced);
-  //fChain->SetBranchAddress("reducedhits.", fHitsReduced.);
-			   
+  fChain->SetBranchAddress("nhits", &fNHits);
+  fChain->SetBranchAddress("hit.detid", &fHitDetID);
+  fChain->SetBranchAddress("hit.chan", &fHitChan);
+  fChain->SetBranchAddress("hit.pos", &fHitPos);
+  fChain->SetBranchAddress("hit.tdc", &fHitTDC);
+  fChain->SetBranchAddress("hit.drift", &fHitDrift);
+  
+  fChain->SetBranchAddress("ntracks", &fNTracks);
+  fChain->SetBranchAddress("track.stid", &fTrackStID);
+  fChain->SetBranchAddress("track.nhits", &fTrackNHits);
+  fChain->SetBranchAddress("track.chi2", &fTrackChi2);
+  fChain->SetBranchAddress("track.x0", &fTrackX0);
+  fChain->SetBranchAddress("track.y0", &fTrackY0);
+  fChain->SetBranchAddress("track.tx", &fTrackTX);
+  fChain->SetBranchAddress("track.ty", &fTrackTY);
+  fChain->SetBranchAddress("track.invp", &fTrackInvP);
+  fChain->SetBranchAddress("track.err_x0", &fTrackErrX0);
+  fChain->SetBranchAddress("track.err_y0", &fTrackErrY0);
+  fChain->SetBranchAddress("track.err_tx", &fTrackErrTX);
+  fChain->SetBranchAddress("track.err_ty", &fTrackErrTY);
+  fChain->SetBranchAddress("track.err_invp", &fTrackErrInvP);
+  fChain->SetBranchAddress("track.charge", &fTrackCharge);
+  fChain->SetBranchAddress("track.hitdetid", &fTrackHitsDetID);
+  fChain->SetBranchAddress("track.hitchan", &fTrackHitsChan);
+  fChain->SetBranchAddress("track.hitpos", &fTrackHitsPos);
+  fChain->SetBranchAddress("track.hittdc", &fTrackHitsTDC);
+  fChain->SetBranchAddress("track.hitdrift", &fTrackHitsDrift);
+  fChain->SetBranchAddress("track.vx", &fTrackVx);
+  fChain->SetBranchAddress("track.vy", &fTrackVy);
+  fChain->SetBranchAddress("track.vz", &fTrackVz);
+  fChain->SetBranchAddress("track.px", &fTrackPx);
+  fChain->SetBranchAddress("track.py", &fTrackPy);
+  fChain->SetBranchAddress("track.pz", &fTrackPz);
 }
 
 void ORoutput_tree::Clear()
 {
-  for(Int_t i = 0; i < nChamberPlanes+nHodoPlanes+nPropPlanes+1; i++)
-    {
-      fNhitsReduced[i] = 0;
-    }
+  fNHits = 0;
+  fHitDetID.clear();
+  fHitChan.clear();
+  fHitPos.clear();
+  fHitTDC.clear();
+  fHitDrift.clear();
+  fNTracks = 0;
+  fTrackStID.clear();
+  fTrackNHits.clear();
+  fTrackChi2.clear();
+  fTrackX0.clear();
+  fTrackY0.clear();
+  fTrackTX.clear();
+  fTrackTY.clear();
+  fTrackInvP.clear();
+  fTrackErrX0.clear();
+  fTrackErrY0.clear();
+  fTrackErrTX.clear();
+  fTrackErrTY.clear();
+  fTrackErrInvP.clear();
+  fTrackCharge.clear();
+  fTrackHitsDetID.clear();
+  fTrackHitsChan.clear();
+  fTrackHitsPos.clear();
+  fTrackHitsTDC.clear();
+  fTrackHitsDrift.clear();
 }
 
 void ORoutput_tree::Write()
 {
   fChain->Write("");
-  //fChain->SetBranchAddress("reducedhits.", fHitsReduced.);
-			   
 }
--- OnlineReconstruction/OROutput.h
+++ OnlineReconstruction/OROutput.h
@@ -16,12 +16,75 @@ public:
   void Write();
   
   TChain* fChain;
-  Int_t fNhitsReduced[nChamberPlanes+nHodoPlanes+nPropPlanes+1];
-  //std::vector<Hit> fHitsReduced;
+  Int_t fNHits;
+  std::vector<int> fHitDetID;
+  std::vector<int> fHitChan;
+  std::vector<float> fHitPos;
+  std::vector<float> fHitTDC;
+  std::vector<float> fHitDrift;
+
+  Int_t fNTracks;
+  std::vector<int> fTrackStID;
+  std::vector<int> fTrackNHits;
+  std::vector<float> fTrackChi2;
+  std::vector<float> fTrackX0;
+  std::vector<float> fTrackY0;
+  std::vector<float> fTrackTX;
+  std::vector<float> fTrackTY;
+  std::vector<float> fTrackInvP;
+  std::vector<float> fTrackErrX0;
+  std::vector<float> fTrackErrY0;
+  std::vector<float> fTrackErrTX;
+  std::vector<float> fTrackErrTY;
+  std::vector<float> fTrackErrInvP;
+  std::vector<int> fTrackCharge;
+  std::vector<int> fTrackHitsDetID;
+  std::vector<int> fTrackHitsChan;
+  std::vector<float> fTrackHitsPos;
+  std::vector<float> fTrackHitsTDC;
+  std::vector<float> fTrackHitsDrift;
+  std::vector<float> fTrackVx;
+  std::vector<float> fTrackVy;
+  std::vector<float> fTrackVz;
+  std::vector<float> fTrackPx;
+  std::vector<float> fTrackPy;
+  std::vector<float> fTrackPz;
+
+  /*
+  TBranch* b_NHits;
+  TBranch* b_HitDetID;
+  TBranch* b_HitChan;
+  TBranch* b_HitPos;
+  TBranch* b_HitTDC;
+  TBranch* b_HitDrift;
   
-// private:
-//   TBranch* b_nhitsreduced;
-//   TBranch* b_hitsreduced;
+  TBranch* b_NTracks;
+  TBranch* b_TrackStID;
+  TBranch* b_TrackNHits;
+  TBranch* b_TrackChi2;
+  TBranch* b_TrackX0;
+  TBranch* b_TrackY0;
+  TBranch* b_TrackTX;
+  TBranch* b_TrackTY;
+  TBranch* b_TrackInvP;
+  TBranch* b_TrackErrX0;
+  TBranch* b_TrackErrY0;
+  TBranch* b_TrackErrTX;
+  TBranch* b_TrackErrTY;
+  TBranch* b_TrackErrInvP;
+  TBranch* b_TrackCharge;
+  TBranch* b_TrackHitsDetID;
+  TBranch* b_TrackHitsChan;
+  TBranch* b_TrackHitsPos;
+  TBranch* b_TrackHitsTDC;
+  TBranch* b_TrackHitsDrift;
+  TBranch* b_TrackVx;
+  TBranch* b_TrackVy;
+  TBranch* b_TrackVz;
+  TBranch* b_TrackPx;
+  TBranch* b_TrackPy;
+  TBranch* b_TrackPz;
+  */
   
   ClassDef(ORoutput_tree, 1)
 }; 
